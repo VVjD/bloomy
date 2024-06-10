56ef464283c928f7c8bd66aab86b85673846af76
jub <js4real@naver.com>
2024-06-10 17:24:31 +0900

    프로젝트 생성

---
+++ bloomy/src/main/java/com/test/bloomy/BloomyApplication.java
@@ -0,0 +1,13 @@
+package com.test.bloomy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloomyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BloomyApplication.class, args);
+    }
+
+}
